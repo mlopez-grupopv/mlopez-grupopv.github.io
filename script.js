ab0aa60c6dcf3828333acf6afdbb15809aa970bb
Michel Lopez <mlopez@grupopv.mx>
2022-04-05 11:20:44 -0600

    Add input data to form

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ function pdfexport(){
     //variables
     //labels
     var ltitle = document.getElementById('tlt').innerText,
-        lname = document.getElementById('lbname').innerText,
-        lage = document.getElementById('lbage').innerText,
-        ldob = document.getElementById('lbdob').innerText,
-        lover = document.getElementById('lbover').innerText;
+        ldes = document.getElementById('lbdes').innerText,
+        lmod = document.getElementById('lbmod').innerText,
+        lmar = document.getElementById('lbmar').innerText,
+        lped = document.getElementById('lbped').innerText,
+        lfec = document.getElementById('lbfec').innerText,
+        lpde = document.getElementById('lbpde').innerText,
+        lori = document.getElementById('lbori').innerText;
     //input
-    var inname = document.getElementById('inname').value,
-        inage = document.getElementById('inage').value,
-        indob = document.getElementById('indob').value,
-        ininfo = document.getElementById('inover').value;
+    var indes = document.getElementById('indes').value,
+        inmod = document.getElementById('inmod').value,
+        inmar = document.getElementById('inmar').value;
+        inped = document.getElementById('inped').value,
+        infec = document.getElementById('infec').value,
+        inpde = document.getElementById('inpde').value,
+        inori = document.getElementById('inori').value;
 
     //jspdf part
     var doc = new jsPDF();
@@ -19,9 +25,12 @@ function pdfexport(){
     doc.setFontSize(22);
     doc.text(ltitle, 20, 10);
     doc.setFontSize(16);
-    doc.text(lname + '  ' + inname, 10, 20);
-    doc.text(lage + ' ' + inage, 10, 30);
-    doc.text(ldob + ' ' + indob, 10, 40);
-    doc.text(lover + ' ' + ininfo, 10, 50);
-    doc.output('dataurlnewwindow',{filename: inname.value});
+    doc.text(ldes + '  ' + indes, 10, 20);
+    doc.text(lmod + ' ' + inmod, 10, 30);
+    doc.text(lmar + ' ' + inmar, 10, 40);
+    doc.text(lped + ' ' + inped, 10, 50);
+    doc.text(lfec + ' ' + infec, 10, 60);
+    doc.text(lpde + ' ' + inpde, 10, 70);
+    doc.text(lori + ' ' + inori, 10, 80);
+    doc.output('dataurlnewwindow',{filename: indes.value});
 }
